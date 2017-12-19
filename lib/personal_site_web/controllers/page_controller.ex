21bbbafdc0fab469c25f3d2234d716958274302f
Anup Vasudevan <rtre84@users.noreply.github.com>
2017-12-18 20:58:29 -0500

    First commit.

---
+++ lib/personal_site_web/controllers/page_controller.ex
@@ -0,0 +1,11 @@
+defmodule PersonalSiteWeb.PageController do
+  use PersonalSiteWeb, :controller
+
+  def index(conn, _params) do
+    render conn, "index.html"
+  end
+
+  def about(conn, _params) do
+    render conn, "about.html"
+  end
+end
---
+++ lib/personal_site_web/router.ex
@@ -0,0 +1,27 @@
+defmodule PersonalSiteWeb.Router do
+  use PersonalSiteWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", PersonalSiteWeb do
+    pipe_through :browser # Use the default browser stack
+
+    get "/", PageController, :index
+    get "/about", PageController, :about
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", PersonalSiteWeb do
+  #   pipe_through :api
+  # end
+end
